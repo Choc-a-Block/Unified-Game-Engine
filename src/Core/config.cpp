e1593ba8ce87624ef2a0413ef90479a24e363b18
Choc-a-Block <80892986+Choc-a-Block@users.noreply.github.com>
2024-06-03 14:44:38 +0000

    Deploying to gh-pages from @ Choc-a-Block/Unified-Game-Engine@bde713c0b95548e900593e2912ca7c82698f21a3 🚀

--- src/Core/config.cpp
+++
@@ -1,5 +0,0 @@
-#include <Unified-Engine/Core/config.h>
-
-using namespace UnifiedEngine;
-
-GlobalConfig UnifiedEngine::__GLOBAL_CONFIG__ = {};
--- src/Objects/Components/collider.cpp
+++
@@ -1,3 +0,0 @@
-#include <Unified-Engine/Objects/Components/collider.h>
-
-using namespace UnifiedEngine;
